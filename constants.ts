59ec542ef2b656b4f23064da20572130c935d8a0
Vishal <vishal15v2006@gmail.com>
2026-03-12 14:04:06 +0530

    fc

--- constants.ts
+++
@@ -1,5 +0,0 @@
-
-export const SET_COLORS = [
-    'bg-sky-200', 'bg-emerald-200', 'bg-amber-200', 'bg-violet-200', 'bg-rose-200',
-    'bg-cyan-200', 'bg-lime-200', 'bg-orange-200', 'bg-purple-200', 'bg-pink-200'
-];
